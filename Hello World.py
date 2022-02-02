1259fe844f5bbe8ad68ef8883ea32b728ba5c1ed
Kgee321 <geek@middleton.school.nz>
2022-02-03 09:31:07 +1300

    Create Hello world
    Testing repository

---
+++ Hello World.py
@@ -0,0 +1,5 @@
+cool = input("What's you're name?")
+if cool == "Katelyn":
+    print("You have the best name EVERRR!!")
+else:
+    print("That name is cool... but not as cool as the name KATELYN!")
